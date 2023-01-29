f84e2fef59c2b706ad8609419696b21ef9697395
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-01-29 14:50:37 -0600

    Installing TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.jsx", "./src/Index.jsx", "./src/screens/*.{js,jsx,ts,tsx}", "./src/components/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
