bc34fb208dba4b92dd5bd9ea20c00a49876d7639
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-01-29 17:11:50 -0600

    Building Place object.

--- src/components/FieldForm.jsx
+++ src/components/FieldForm.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react"
 import { View, Text, TextInput } from "react-native"
 
-export default function FieldForm({question}){
+export default function FieldForm({question, name, buildPlace}){
+  const [ value, setValue ] = useState('');
 
   return(
     <View className="rounded-2xl ">
       <Text className="mt-3 mb-1 font-semibold font-bold text-gray-600">{question}</Text>
-      <TextInput className="border border-gray-300 rounded-lg" />
+      <TextInput className="border border-gray-300 rounded-lg px-2 py-1" onChangeText={buildPlace.bind(value, name)} />
     </View>
   )
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,29 +1,29 @@
 import { useState } from "react";
 import { View } from "react-native";
+
 import CustomButton from "./CustomButton";
 import FieldForm from "./FieldForm";
 
 export default function Form(){
-  const [place, setPlace] = useState({
-    name: '', imageURL: '', location: ''
-  });
+  let [place, setPlace] = useState({name: '', imageURL: '', location: ''});
 
-  function buildPlace(id, value){
-    // console.log(id);
-    // console
+  function buildPlace(name, value){
+    setPlace(previousState => ({
+      ...previousState, [name]: value
+    }))
   }
 
   function savePlace(){
-
+    console.log(`Your place is: ${place.name}`)
   }
 
   console.log(place)
 
   return(
     <View className="bg-white p-2 m-2 rounded-lg">
-      <FieldForm question="Place's name"/>
-      <FieldForm question="Place's image"/>
-      <FieldForm question="Place's location"/>
+      <FieldForm question="Place's name" name="name" buildPlace={buildPlace} />
+      <FieldForm question="Place's image" name="imageURL" buildPlace={buildPlace} />
+      <FieldForm question="Place's location" name="location" buildPlace={buildPlace} />
       <View className="items-center mt-10 mb-5">
         <CustomButton title="Create place" action={savePlace} />
       </View>
