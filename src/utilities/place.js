71e73d5e2844a6d1c607afec5b0912a6c825bb2b
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-01-29 15:13:53 -0600

    Creating Place Class and Installing Jotai package.

---
+++ src/utilities/place.js
@@ -0,0 +1,10 @@
+class Place {
+  constructor(id, name, imageURL, location) {
+    this.id = id;
+    this.name = name;
+    this.imageURL = imageURL;
+    this.location = location;
+  }
+}
+
+export default Place;
